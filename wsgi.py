1b36a14a9bf3167bb55898d54fa9dd49f9ce189f
minho <pot1223@naver.com>
2025-09-02 12:51:04 +0900

    DB commit_2

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-config_key = os.getenv("CONFIG_KEY")
+config_key = os.getenv("dev")
 app = create_app(config_key)# waitress 서버로 애플리케이션 실행
 
 if __name__ == "__main__":
